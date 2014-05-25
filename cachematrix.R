ecf8c69eb412d121da0bfa1fe408ca12d49076fe
Danil <mrpablito@users.noreply.github.com>
2014-05-25 09:54:23 +0300

    Changed indent to 4 spaces.

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,18 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  
-  setInverse <- function(inv) m <<- inv
-  getInverse <- function() m
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    
+    setInverse <- function(inv) m <<- inv
+    getInverse <- function() m
+    list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 ## This function computes the inverse of the special
@@ -21,13 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed),
 ## then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  m <- x$getInverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setInverse(m)
-  m
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
